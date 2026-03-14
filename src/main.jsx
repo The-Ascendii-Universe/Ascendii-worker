3f48e8aa96419f24ce01329a3f538c85b12b86c3
Bryan Savage <sav79tech@gmail.com>
2026-03-14 11:12:56 -0700

    Initialize main.jsx with React and Model component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Model from './Components/Model.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Model />
+  </React.StrictMode>,
+)
